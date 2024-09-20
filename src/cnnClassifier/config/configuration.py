aa84835b2954c99b6eda9db4dc71661c1238bd52
Goindi7 <manpreetgoind@gmail.com>
2024-09-20 12:10:28 +0530

    copied everything from the repo to know the error

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -2,7 +2,11 @@ from cnnClassifier.constants import *
 import os
 from pathlib import Path
 from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig,PrepareCallbacksConfig,TrainingConfig,EvaluationConfig)
+from cnnClassifier.entity.config_entity import (DataIngestionConfig,
+                                                PrepareBaseModelConfig,
+                                                PrepareCallbacksConfig,
+                                                TrainingConfig,
+                                                EvaluationConfig)
 
 
 
